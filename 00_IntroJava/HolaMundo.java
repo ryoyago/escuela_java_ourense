fa25582245a52a0119e207a2412d55df68c808ff
Yago <kelevra942@gmail.com>
2019-11-06 15:52:06 +0100

    -Empezando: HolaMundo.java

---
+++ 00_IntroJava/HolaMundo.java
@@ -0,0 +1,10 @@
+class HolaMundo {
+	public static void main(String[] argumentos){
+		byte unByte = 100;
+		int unEntero=1000;
+		System.out.println("Hola Ourense! Tipos de datos: ");
+		System.out.println("- Byte: "+ unByte);
+		System.out.println("- int: "+ unEntero);
+		//return 0;
+	}
+}
